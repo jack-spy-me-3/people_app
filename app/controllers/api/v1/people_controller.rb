706fa5b221dde6a02c7fedc43b9989dec401bc97
Samantha Morco <samanthamorco@gmail.com>
2017-03-01 16:21:45 -0500

    add happy/sad paths to rails controller

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -12,22 +12,23 @@ class Api::V1::PeopleController < ApplicationController
     end
   end
 
-  def create
-    @person = Person.create(name: params[:name], bio: params[:bio])
-    render "show.json.jbuilder"
-  end
-
   def update
     @person = Person.find_by(id: params[:id])
     @person.name = params[:name] || @person.name
     @person.bio = params[:bio] || @person.bio
-    @person.save
-    render "show.json.jbuilder"
+    if @person.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @person.errors.full_messages }, status: 422
+    end
   end
 
   def destroy
     @person = Person.find_by(id: params[:id])
-    @person.destroy
-    render json: {message: "Goodbye! This person has died!"}
+    if @person.destroy
+      render json: {message: "Goodbye! This person has died!"}
+    else
+      render json: { errors: @person.errors.full_messages }, status: 422
+    end
   end
 end
