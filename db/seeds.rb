697bfd4c4c03099e79469cad4942896145edd640
Samantha Morco <samanthamorco@gmail.com>
2017-03-13 10:56:36 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Person.create(name: "Bobby Smith", bio: "Edison bulb pitchfork jean shorts green juice lumbersexual, air plant banjo iceland lyft cred squid tattooed health goth.")
-Person.create(name: "Apple Smith", bio: "Edison bulb pitchfork jean shorts green juice lumbersexual, air plant banjo iceland lyft cred squid tattooed health goth.")
-Person.create(name: "Jack Smith", bio: "Edison bulb pitchfork jean shorts green juice lumbersexual, air plant banjo iceland lyft cred squid tattooed health goth.")
+Person.create!([
+  {name: "Apple Martini", bio: "Gwyneth what were you thinking?", age: 3},
+  {name: "Mr. Yummy", bio: "Sandwich", age: 17},
+  {name: "Jack Smith", bio: "Edison bulb pitchfork jean shorts green juice lumbersexual, air plant banjo iceland lyft cred squid tattooed health goth.", age: 60},
+  {name: "Bobby Smith", bio: "Next level dreamcatcher raclette letterpress mixtape meggings godard, ennui squid beard direct trade hella. Humblebrag ugh la croix migas artisan.", age: 12},
+  {name: "Apple Smith", bio: "PBR Blog health goth af brooklyn, affogato hammock readymade live-edge enamel pin aesthetic paleo venmo pok pok biodiesel migas. Brunch 8-bit truffaut pitchfork, cred raclette austin pinterest ramps occupy crucifix vegan copper mug letterpress.", age: 16},
+  {name: "Harvey Jones", bio: "Some cool dude.", age: 21},
+  {name: "Hank", bio: "Makes really juicy beef", age: 33},
+  {name: "Harry Potter", bio: "The boy who lived", age: 35},
+  {name: "Adult", bio: "This is an adult.", age: 34},
+  {name: "Child", bio: "This is a child", age: 9}
+])
